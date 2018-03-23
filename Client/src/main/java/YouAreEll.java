c04a49c3f7ec63a82b5b74e6a03870b999fa5323
Kthomas <kthomas@github.com>
2018-03-23 09:52:24 -0400

    Chose Apache HTTP

--- Client/src/main/java/YouAreEll.java
+++ Client/src/main/java/YouAreEll.java
@@ -1,3 +1,19 @@
+/**
+ * @GOOD&BAD
+ * Deserialization is performed by specialized libraries like Jackson
+ * 1.)Apache is easiest to use for building your own REST client
+ * 2.)Using jackson for json parsing instead of Gson
+ * 3.)Apache has HTTP2 and NIO support
+ * 4.)Apache can establish a pool connection for a host
+ * 5.)Apache has just about every useful function you will ever need
+ * 6.)Apache is one of the fastest asynchronous (non-blocking) clients out there
+ * 7.)Apache is very reliable, basic, portable, and usable
+ * 8.)Apache has it's own middleware stack (API) and implements a Fetch API
+ * 9.)Apache is not a browser compatible as other clients
+ * 10.)Very High latency
+ */
+
+
 public class YouAreEll {
 
     YouAreEll() {
